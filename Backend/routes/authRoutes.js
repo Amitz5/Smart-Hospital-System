eff7a6de6c2a4ee9259844264c54710f8e038525
Amitz5 <amitozsingh475@gmail.com>
2026-04-15 17:15:39 +0530

    route fix

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -7,8 +7,8 @@ const { verifyOtp } = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtp);
+// router.post("/send-otp", sendOtp);
+// router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 
 router.post(
